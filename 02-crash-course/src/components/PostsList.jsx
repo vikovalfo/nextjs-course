d6fe50488a125a759bbdd5e7954085f9ee517699
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-12-14 12:58:56 -0600

    state and conditionals

--- 02-crash-course/src/components/PostsList.jsx
+++ 02-crash-course/src/components/PostsList.jsx
@@ -3,20 +3,23 @@ import { useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostsList.module.css";
-import Modal from '../ui/Modal';
+import Modal from "../ui/Modal";
 
 const PostsList = () => {
+  const [isModalVisible, setIsModalVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
 
   return (
     <>
-      <Modal>
-        <NewPost
-          onAddAuthor={(author) => setEnteredAuthor(author)}
-          onAddBody={(body) => setEnteredBody(body)}
-        />
-      </Modal>
+      {isModalVisible && (
+        <Modal onClose={() => setIsModalVisible(false)}>
+          <NewPost
+            onAddAuthor={(author) => setEnteredAuthor(author)}
+            onAddBody={(body) => setEnteredBody(body)}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Dave" body="No, this is sparta!" />
