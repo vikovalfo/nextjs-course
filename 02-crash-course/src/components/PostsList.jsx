378870337f983837338a569157c7e490ea227cea
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-12-14 14:43:07 -0600

    formatting and sending posts data to BE

--- 02-crash-course/src/components/PostsList.jsx
+++ 02-crash-course/src/components/PostsList.jsx
@@ -8,7 +8,14 @@ import Modal from "../ui/Modal";
 const PostsList = ({ isPosting, onStopPosting }) => {
   const [posts, setPosts] = useState([]);
 
-  function onAddPostHandler(post) {
+  async function onAddPostHandler(post) {
+    const result = await fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(post),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((prevPosts) => [...prevPosts, post]);
   }
 
@@ -19,11 +26,15 @@ const PostsList = ({ isPosting, onStopPosting }) => {
           <NewPost onAddPost={onAddPostHandler} onCancel={onStopPosting} />
         </Modal>
       )}
-     {(posts.length > 0) ? <ul className={classes.posts}>
-        {posts.map(({ author, body }) => (
-          <Post key={Math.random()} author={author} body={body} />
-        ))}
-      </ul>: <h3>No post available yet, please add one</h3>}
+      {posts.length > 0 ? (
+        <ul className={classes.posts}>
+          {posts.map(({ author, body }) => (
+            <Post key={Math.random()} author={author} body={body} />
+          ))}
+        </ul>
+      ) : (
+        <h3>No post available yet, please add one</h3>
+      )}
     </>
   );
 };
