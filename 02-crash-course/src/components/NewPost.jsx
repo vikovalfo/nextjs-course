b5a1d93939a5c308ad3e1ac85fd100668e7fd633
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-12-14 11:10:06 -0600

    using state

--- 02-crash-course/src/components/NewPost.jsx
+++ 02-crash-course/src/components/NewPost.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
+
 import classes from "./NewPost.module.css";
 
-const NewPost = () => {
+const NewPost = ({ onAdd }) => {
+  const [enteredBody, setEnteredBody] = useState('');
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  };
+
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} value={enteredBody}/>
       </p>
       <p>
         <label htmlFor="name">Your name</label>
