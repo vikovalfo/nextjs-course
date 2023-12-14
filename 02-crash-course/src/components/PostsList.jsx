272eb7a773d895e4654ce44e0d9ad7a3b1c68699
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-12-14 14:57:34 -0600

    useEffect and fetching data from BE

--- 02-crash-course/src/components/PostsList.jsx
+++ 02-crash-course/src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Post from "./Post";
 import NewPost from "./NewPost";
@@ -8,8 +8,18 @@ import Modal from "../ui/Modal";
 const PostsList = ({ isPosting, onStopPosting }) => {
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const data = await response.json();
+      setPosts(data.posts);
+    }
+
+    fetchPosts();
+  }, []);
+
   async function onAddPostHandler(post) {
-    const result = await fetch("http://localhost:8080/posts", {
+    await fetch("http://localhost:8080/posts", {
       method: "POST",
       body: JSON.stringify(post),
       headers: {
