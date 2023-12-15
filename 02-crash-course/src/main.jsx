223ae9ca5d1e93483b1803e64d6bce2ec2a4950a
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-12-15 13:25:41 -0600

    layout routes

--- 02-crash-course/src/main.jsx
+++ 02-crash-course/src/main.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
+import NewPost from "./components/NewPost";
+import RootLayout from "./pages/RootLayout";
 
 const router = createBrowserRouter([
-  {path: '/', element:<App />}
+  {
+    path: '/', element: <RootLayout />, children: [
+      { index: true, element: <App /> },
+      { path: "/create-post", element: <NewPost /> },
+  ]},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
