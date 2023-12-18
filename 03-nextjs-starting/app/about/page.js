9ca7cfb99610a094ac18c06cd9bc0d635a486e44
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-12-18 13:46:27 -0600

    re-using components and basic routing

--- 03-nextjs-starting/app/about/page.js
+++ 03-nextjs-starting/app/about/page.js
@@ -1,9 +1,7 @@
 export default function About() {
     return (
         <main>
-            <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-            <h1>Welcome to this NextJS Course!</h1>
-            <p>🔥 Let&apos;s get started! 🔥</p>
+            <p>About</p>
         </main>
     );
 }
