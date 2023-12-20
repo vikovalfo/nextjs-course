60790c658637deaa38ecd2b37ff9f98315eca22a
Alessa Valencia <alessa.valenciafosado@cognizant.com>
2023-12-20 13:16:42 -0600

    not found mgmt

---
+++ 04-foodies-app/app/not-found.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <main className="not-found">
+            <p>Not found, we couldn't find the requested resource or page.</p>
+        </main>
+    );
+}
